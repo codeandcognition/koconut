32bd03eea7db28ecefa3a63f36fe3843bf9d90cf
Alex Tan <alextan@uw.edu>
2017-07-05 11:14:38 -0700

    Installed Storybook add-on Info

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,3 +1,12 @@
+import { configure, setAddon } from '@storybook/react';
+import infoAddon from '@storybook/addon-info';
+
+setAddon(infoAddon);
+
+configure(function () {
+    //...
+}, module);
+
 import React from 'react';
 
 import {storiesOf} from '@storybook/react';
@@ -5,6 +14,8 @@ import {action} from '@storybook/addon-actions';
 import {linkTo} from '@storybook/addon-links';
 
 import {Button, Welcome} from '@storybook/react/demo';
+
+import App from '../App';
 
 storiesOf('Welcome', module).
     add('to Storybook', () => <Welcome showApp={linkTo('Button')}/>);
@@ -14,3 +25,12 @@ storiesOf('Button', module).
         () => <Button onClick={action('clicked')}>Hello Button</Button>).
     add('with some emoji',
         () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('App', module)
+    .addWithInfo(
+        'Default App Component',
+        'Simple usage of the App component. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad, beatae distinctio et fugiat molestiae natus nihil quasi tenetur voluptatibus. A aperiam dolorum et facilis id ipsam nulla repellat saepe?',
+        () => (
+            <App prop="property" />
+        ),
+    );
