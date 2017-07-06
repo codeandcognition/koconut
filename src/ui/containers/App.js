2aac5601ad5a7415eb82e2a619cc2dce391575b9
Harrison Kwik <resprite@gmail.com>
2017-07-05 17:20:56 -0700

    Fixed flow issues

--- src/ui/containers/App.js
+++ src/ui/containers/App.js
@@ -1,28 +1,35 @@
+// @flow
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 /**
  * This is a component.
+ * @class
  */
 class App extends Component {
+  test(a: number, b: number): number {
+    let x: number = a + b;
+    return x;
+  }
 
-    /**
-     * Greetings globe!
-     */
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
-    }
+  /**
+   * Greetings globe!
+   */
+  render() {
+    return (
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            This is a test {this.test(1, 2)}
+          </p>
+        </div>
+    );
+  }
 }
 
 export default App;
