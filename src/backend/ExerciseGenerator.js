1bcce7d8ac1c2f3d11ea95a08d4f5ae3f74dcb4e
Leanne Hwa <lkhwa@uw.edu>
2017-07-24 11:49:15 -0700

    Get exercise type

--- src/backend/ExerciseGenerator.js
+++ src/backend/ExerciseGenerator.js
@@ -1,5 +1,6 @@
 // @flow
 import {exampleExercises} from '../data/Exercises.js';
+import ExerciseTypes from '../data/ExerciseTypes.js';
 import MasteryModel from '../data/MasteryModel';
 
 class ExerciseGenerator {
@@ -9,11 +10,23 @@ class ExerciseGenerator {
     this.counter = 0;
   }
 
-  getConcept():string {
+  /**
+   * Returns a concept for the exercise, pulled from mastery model.
+   * @returns an exercise concept
+   */
+  getConcept(): string {
     let model = MasteryModel.model;
-    return model[Math.random()*model.length].concept;
+    return model[Math.random() * model.length].concept;
   }
 
+  /**
+   * Returns an exercise type, pulled from exercise type inventory.
+   * @returns an exercise type
+   */
+  getType(): string {
+    let types = Object.keys(ExerciseTypes);
+    return types[Math.random() * types.length];
+  }
   /**
    * Returns a generated Exercise
    * @returns {Prompt} - a generated Exercise
