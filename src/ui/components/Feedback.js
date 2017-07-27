0c2643482a8f63c13f7ae922973cd2e1c96bacdf
Leanne Hwa <lkhwa@uw.edu>
2017-07-27 15:41:01 -0700

    Initial feedback component

---
+++ src/ui/components/Feedback.js
@@ -0,0 +1,21 @@
+// @flow
+import React, {Component} from 'react';
+import './Feedback.css';
+
+/**
+ * Component that displays a feedback modal after user submits an answer.
+ *
+ */
+
+class Feedback extends Component {
+
+  render() {
+
+    return (
+      <div className="feedback">
+
+      </div>
+    )
+
+  }
+}
