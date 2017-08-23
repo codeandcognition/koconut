bbb985ba38be7984157d5f8c84363e1413523abd
alexhuitan <alextan@uw.edu>
2017-08-22 17:22:14 -0700

    Cleaned up BKT parameters

--- src/backend/BKT.js
+++ src/backend/BKT.js
@@ -1,27 +1,33 @@
 //@flow
 
+/**
+ * Class for Bayesian Knowledge Tracing functionality.
+ */
 class BKT {
-  L: number;
   T: number;
   G: number;
   S: number;
 
+  /**
+   * Construct initial values of BKT model.
+   */
   constructor() {
-    this.L = 0.34; //Corbett
-    this.T = 0.2; //Corbett
+    this.T = 0.2;
     this.G = 0.066; //Constant from Baker et al.
     this.S = 0.402; //Constant from Baker et al.
   }
 
+  /**
+   * Take in previous K value and correct/incorrect answer to update K value.
+   * @param previous
+   * @param correct
+   * @returns {number}
+   */
   learned(previous: number, correct: boolean) {
     let S = correct ? 1 - this.S : this.S;
     let G = correct ? this.G : 1 - this.G;
     let previously = (previous * S) / ((previous * S) + ((1 - previous) * G));
     return previously + (1 - previously) * this.T;
-  }
-
-  printValues() {
-    console.log('L: ' + this.L + '\nT: ' + this.T + '\nG: ' + this.G + '\nS: ' + this.S);
   }
 }
 
