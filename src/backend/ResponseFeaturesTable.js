3ffa646b0211e7e2b5cb4611e470f75f2ea77b08
alexhuitan <alextan@uw.edu>
2017-08-24 14:47:57 -0700

    Factored out contextualized weights

--- src/backend/ResponseFeaturesTable.js
+++ src/backend/ResponseFeaturesTable.js
@@ -1,17 +1,26 @@
 //@flow
 
+/**
+ * Factors out values for contextualized G and S
+ * from Baker et al.
+ */
 const ResponseFeatures = {
-  responseIsString: [0.049, -0.02],
-  numberOfLastFiveWrong: [0.036, -0.033],
-  percentPastErrors: [0, -0.004],
-  helpRequest: [0, 0.066],
-  percentHelpRequested: [0, -0.047],
-  numberOfLast8HelpRequested: [0.042, -0.019],
-  timeTaken: [0.002, -0.0002],
-  timeTakenSD: [-0.024, 0.01],
-  timeTakenInLast5Actions: [-0.003, 0.002],
-  timeTakenOnThisConcept: [0.001, -0.001],
-  numberOfTimesUsingConcept: [0.003, -0.001]
+  responseIsString:             {g: 0.049,  s: -0.02},
+  numberOfLastFiveWrong:        {g: 0.036,  s: -0.033},
+  percentPastErrors:            {g: 0,      s: -0.004},
+  helpRequest:                  {g: 0,      s: 0.066},
+  percentHelpRequested:         {g: 0,      s: -0.047},
+  numberOfLast8HelpRequested:   {g: 0.042,  g: -0.019},
+  timeTaken:                    {g: 0.002,  s: -0.0002},
+  timeTakenSD:                  {g: -0.024, s: 0.01},
+  timeTakenInLast5Actions:      {g: -0.003, s: 0.002},
+  timeTakenOnThisConcept:       {g: 0.001,  s: -0.001},
+  numberOfTimesUsingConcept:    {g: 0.003,  s:-0.001}
 };
 
-export default ResponseFeatures;
+//Index for g in features
+const g = 0;
+//Index for s in features
+const s = 1;
+
+export {ResponseFeatures, g, s};
