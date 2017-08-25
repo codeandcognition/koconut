acea383a1db2e44977e399cbbde237ec577e6cdf
alexhuitan <alextan@uw.edu>
2017-08-25 12:04:41 -0700

    Finished implementing contextual factors

--- src/backend/ResponseFeaturesTable.js
+++ src/backend/ResponseFeaturesTable.js
@@ -25,66 +25,149 @@ const ResponseFeatures = {
  * Determines whether the response object was a String response
  * (WriteCode, FillBlank, or ShortResponse).
  * @param response
- * @param num
- * @param i
  * @returns {boolean} true if response is string
  */
 function responseIsString(response: ResponseObject) {
-  return response.exerciseType === ExerciseTypes.writeCode ||
+  let isString =  response.exerciseType === ExerciseTypes.writeCode ||
       response.exerciseType === ExerciseTypes.fillBlank ||
       response.exerciseType === ExerciseTypes.shortResponse;
+  return isString ? 1 : 0;
 }
 
 /**
  * Determines the percentage of past responses of the same concept that were
  * errors.
  * @param response
- * @param num
- * @param i
  * @returns {number} percentage
  */
 function percentPastErrors(response: ResponseObject) {
-  let responsesOfConcept = ResponseLog.log.filter(
-      (res) => res.concept === response.concept);
-  let incorrectResponses = responsesOfConcept.filter((res) => res.correct === false);
-  return incorrectResponses.length / responsesOfConcept.length;
+  let len = ResponseLog.log.length;
+  if(len > 0) {
+    let responsesOfConcept = ResponseLog.log.filter(
+        (res) => res.concept === response.concept);
+    let incorrectResponses = responsesOfConcept.filter((res) => res.correct === false);
+    return incorrectResponses.length / responsesOfConcept.length;
+  }
+  return 0;
 }
 
 /**
  * Determines the percentage of past responses of the same concept that were
  * errors.
  * @param response
- * @param num
- * @param i
  * @returns {number} percentage
  */
 function numberOfLastFiveWrong(response: ResponseObject) {
+  let len = ResponseLog.log.length;
+  if(len > 4) {
+    let lastFiveResponses = ResponseLog.log.slice(len - 5, len);
+    return lastFiveResponses.filter((res) => res.correct === false).length;
+  }
   return 0;
 }
 
+/**
+ * Stub, no help request feature built in yet.
+ * @param response
+ * @returns {number}
+ */
 function helpRequest(response: ResponseObject) {
   return 0;
 }
+
+/**
+ * Stub, no help request feature built in yet.
+ * @param response
+ * @returns {number}
+ */
 function percentHelpRequested(response: ResponseObject) {
   return 0;
 }
+
+/**
+ * Stub, no help request feature built in yet.
+ * @param response
+ * @returns {number}
+ */
 function numberOfLast8HelpRequested(response: ResponseObject) {
   return 0;
 }
+
+/**
+ * Returns number of minutes spent on most recent exercise.
+ * @param response
+ * @returns {number}
+ */
 function timeTaken(response: ResponseObject) {
+  let len = ResponseLog.log.length;
+  if(len > 1) {
+    let now = ResponseLog.getLastElement();
+    let pre = ResponseLog.log[len - 2];
+    return convertMilliToMin(now.timestamp - pre.timestamp);
+  }
   return 0;
 }
+
+/**
+ * Stub, no collective student data yet.
+ * @param response
+ * @returns {number}
+ */
 function timeTakenSD(response: ResponseObject) {
   return 0;
 }
+
+/**
+ * Returns number of minutes spent on last 5 exercises.
+ * @param response
+ * @returns {number}
+ */
 function timeTakenInLast5Actions(response: ResponseObject) {
+  let len = ResponseLog.log.length;
+  if(len > 5) {
+    let now = ResponseLog.getLastElement();
+    let pre = ResponseLog.log[len - 6];
+    return convertMilliToMin(now.timestamp - pre.timestamp);
+  }
   return 0;
 }
+
+/**
+ * Returns total number of minutes spent on this concept.
+ * @param response
+ * @returns {number}
+ */
 function timeTakenOnThisConcept(response: ResponseObject) {
+  let len = ResponseLog.log.length;
+  if(len > 1) {
+    let relevant = ResponseLog.log.filter((res) => res.concept === response.concept);
+    // Running sum to compute total time taken on this concept
+    return relevant.reduce((sum, res) => {
+      let pos = ResponseLog.log.indexOf(res);
+      let pre = ResponseLog.log[pos - 1];
+      let time = convertMilliToMin(res.timestamp - pre.timestamp);
+      return sum + time;
+    }, 0);
+  }
   return 0;
 }
+
+/**
+ * Returns number of times using this concept.
+ * @param response
+ * @returns {number}
+ */
 function numberOfTimesUsingConcept(response: ResponseObject) {
-  return 0;
+  return ResponseLog.log.filter((res) => res.concept === response.concept).length;
+}
+
+/**
+ * Helper function to convert milliseconds to minutes
+ * @param time
+ * @returns {number}
+ */
+function convertMilliToMin(time: number) {
+  return time/1000/60;
 }
 
 //Index for g in features
