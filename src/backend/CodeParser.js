1d296d0e46dd8df0f4cc00d7040363f4d289edbb
Harrison Kwik <resprite@gmail.com>
2017-08-31 17:42:42 -0700

    More test code and update eslint config (also technically fixes #18)

--- src/backend/CodeParser.js
+++ src/backend/CodeParser.js
@@ -1,6 +1,8 @@
 // @flow
 import ace from 'brace';
-const { Range } = ace.acequire('ace/range'); // Ace has a built-in range datatype
+import _ from 'lodash';
+
+const {Range} = ace.acequire('ace/range'); // Ace has a built-in range datatype
 
 /*
  * Exercise code can be encoded with special tags to denote ranges/lines:
@@ -23,7 +25,10 @@ class CodeParser {
    */
   static clean(code: string): string {
     // fancy "make a regex of the tags and replace each instance with nothing"
-    return code.replace(new RegExp(Object.keys(_tags).join("|"), 'g'), '');
+    let re = Object.values(_tags).
+        map((tag: any /* hush */) => _.escapeRegExp(tag)).
+        join('|');
+    return code.replace(new RegExp(re, 'g'), '');
   }
 
   /**
@@ -31,31 +36,31 @@ class CodeParser {
    * @param code - the code to extract ranges from
    * @returns an array of ranges/lines
    */
-  static ranges(code: string): Array<Range|number> {
+  static ranges(code: string): Array<Range | number> {
     let ranges = [];
 
     // verbose!
-    let start = { row: undefined, column: undefined };
-    let end = { row: undefined, column: undefined };
+    let start = {row: undefined, column: undefined};
+    let end = {row: undefined, column: undefined};
 
     // I think is the best way to iterate and preserve line number
     let lines = code.split('\n');
-    for(let i = 0; i < lines.length; i++) {
+    for (let i = 0; i < lines.length; i++) {
       // check if each tag is on the line
       Object.values(_tags).forEach((tag) => {
-        if(typeof tag === 'string') {
-          let index = lines[i].search(tag);
+        if (typeof tag === 'string') {
+          let index = lines[i].search(new RegExp(_.escapeRegExp(tag)));
 
           // add range/line data to ranges if tag found
-          if(index !== -1) {
-            switch(tag) {
+          if (index !== -1) {
+            switch (tag) {
               case(_tags.LINE):
                 ranges.push(i);
                 break;
               case(_tags.START):
                 // validity check
-                if(start.row !== undefined || start.column !== undefined) {
-                  throw { message: 'Repeated START tag in code' };
+                if (start.row !== undefined || start.column !== undefined) {
+                  throw new Error('Repeated START tag in code');
                 }
 
                 start.row = i;
@@ -63,19 +68,20 @@ class CodeParser {
                 break;
               case(_tags.END):
                 // validity part 2
-                if(end.row !== undefined || end.column !== undefined) {
-                  throw { message: 'Repeated END tag in code' };
+                if (end.row !== undefined || end.column !== undefined) {
+                  throw new Error('Repeated END tag in code');
                 }
 
                 end.row = i;
                 end.column = index;
 
                 // shift if start and end are on the same line
-                if(start.row === end.row) {
+                if (start.row === end.row) {
                   end.column -= _tags.START.length;
                 }
 
-                ranges.push(new Range(start.row, start.column, end.row, end.column));
+                ranges.push(
+                    new Range(start.row, start.column, end.row, end.column));
 
                 // reset start and end
                 start = {};
