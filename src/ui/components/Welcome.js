b83d6bc8e902db1ce03a474482d5c5b3c0034c32
alexhuitan <alextan@uw.edu>
2017-09-08 16:05:28 -0700

    Store all strings in json

--- src/ui/components/Welcome.js
+++ src/ui/components/Welcome.js
@@ -2,49 +2,49 @@
 import React, {Component} from 'react';
 import './Welcome.css';
 
+const strings = {
+  welcome: "Welcome to Koconut!",
+  intro: "This is an intelligent tutoring system. We're doing this to help you learn introductory programming. We'll keep track of your knowledge, and make recommendations. We also ask for your feedback in the interest of making this learning experience collaborative.",
+  use_cases: {
+    title: "Use Cases",
+    cases: ["Study for your test", "Review material", "Challenge yourself",
+      "Infinite practice (WIP)"]
+  },
+  user_modes: {
+    title: "User Modes",
+    modes: ["Student driven (total freedom)",
+      "Negotiated (we choose some, you choose some)",
+      "Guided learning -- you're not sure what to study"]
+  },
+  language: {
+    desc: "Language: Java",
+    rationale: "We're using Java because it's the introductory language in University of Washington CS courses.",
+    example_desc: "This is an example of Java code:",
+    example_code: ["int x = 1337;", "System.out.println(\"Hi!\");", "System.out.println(\"My name is \" + x);"]
+  },
+  nsf: "this project is NSF funded",
+  agreement: "I have read and understand the purposes of this program.",
+  iagree: "I Agree"
+}
+
 class Welcome extends Component {
 
   render() {
     return (
       <div className="welcome-page">
-        <h2>Welcome to Koconut!</h2>
-
-        <p>
-          This is an intelligent tutoring system.
-          We're doing this to help you learn introductory programming.
-          We'll keep track of your knowledge, and make recommendations.
-          We also ask for your feedback in the interest of making this learning experience collaborative.
-        </p>
-
-        <h4>Use Cases:</h4>
-        <ul>
-          <li>Study for your test</li>
-          <li>Review material</li>
-          <li>Challenge yourself</li>
-          <li>Infinite practice (WIP)</li>
-        </ul>
-
-        <h4>User Modes</h4>
-        <ol>
-          <li>Student driven -- total freedom</li>
-          <li>Negotiated -- we choose some, you choose some</li>
-          <li>Guided learning -- you're not sure what to study</li>
-        </ol>
-
-        <h4>Language: Java</h4>
-        <p>We're using Java because it's the introductory language in University of Washington CS courses.</p>
-
-        <h4>This is an example of Java code:</h4>
-
-        <code>int x = 1337;</code><br/>
-        <code>System.out.println("So " + x);</code>
-
-        <h4>
-          <i>this project is NSF funded</i>
-        </h4>
-
-        <p>I have read and understand the purposes of this program.</p>
-        <button onClick={this.props.callBack}>I Agree</button>
+        <h2>{strings.welcome}</h2>
+        <p>{strings.intro}</p>
+        <h4>{strings.use_cases.title}</h4>
+        <ul>{strings.use_cases.cases.map((c) => <li>{c}</li>)}</ul>
+        <h4>{strings.user_modes.title}</h4>
+        <ol>{strings.user_modes.modes.map((m) => <li>{m}</li>)}</ol>
+        <h4>{strings.language.desc}</h4>
+        <p>{strings.language.rationale}</p>
+        <h4>{strings.language.example_desc}</h4>
+        {strings.language.example_code.map((c) => <div><code>{c}</code></div>)}
+        <h4><i>{strings.nsf}</i></h4>
+        <p>{strings.agreement}</p>
+        <button onClick={this.props.callBack}>{strings.iagree}</button>
       </div>)
   }
 }
