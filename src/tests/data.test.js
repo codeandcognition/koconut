2ec2401c3773526324c09a583b5f51638f270fa0
Harrison Kwik <resprite@gmail.com>
2017-09-14 17:35:02 -0700

    Fixed test

--- src/tests/data.test.js
+++ src/tests/data.test.js
@@ -10,8 +10,8 @@ function toCheck(c) {
 test('ConceptMap parents consistency', () => {
   _.forEach(conceptInventory, (concept, name) => {
     concept.dependencies.filter(c => toCheck(c)).forEach((dependency) => {
-      if(!concept.parents.includes(name)) console.log(`${dependency} missing ${name}`);
-      expect(concept.parents).toContain(name);
+      if(!conceptInventory[dependency].parents.includes(name)) console.log(`${dependency} missing ${name}`);
+      expect(conceptInventory[dependency].parents).toContain(name);
     });
   });
 });
@@ -19,8 +19,8 @@ test('ConceptMap parents consistency', () => {
 test('ConceptMap dependencies consistency', () => {
   _.forEach(conceptInventory, (concept, name) => {
     concept.parents.filter(c => toCheck(c)).forEach((parent) => {
-      if(!concept.dependencies.includes(name)) console.log(`${parent} missing ${name}`);
-      expect(concept.dependencies).toContain(name);
+      if(!conceptInventory[parent].dependencies.includes(name)) console.log(`${parent} missing ${name}`);
+      expect(conceptInventory[parent].dependencies).toContain(name);
     });
   });
 });
