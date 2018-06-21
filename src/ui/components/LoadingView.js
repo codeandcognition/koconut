0575e5d81655f9145c2e519962b579f344c14abe
Jane <janeq97@uw.edu>
2018-06-21 11:27:44 -0700

    Updated LoadingView component

--- src/ui/components/LoadingView.js
+++ src/ui/components/LoadingView.js
@@ -1,9 +1,14 @@
 //@flow
 import React, {Component} from 'react';
+import CircularProgress from 'material-ui/CircularProgress';
 
 
 class LoadingView extends Component {
-
+  render() {
+    return(
+        <CircularProgress />
+    );
+  }
 }
 
 export default LoadingView;
