2177253a4653666bc093c6a708c5c861daaa00f5
William Kwok <wkwok16@uw.edu>
2018-07-17 13:50:24 -0700

    Added filtering

--- src/ui/koconut/containers/WorldView.js
+++ src/ui/koconut/containers/WorldView.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {ConceptKnowledge, MasteryModel} from '../../../data/MasteryModel';
 import {conceptInventory} from '../../../data/ConceptMap.js';
 import ConceptCard from './../components/ConceptCard';
+import {t} from '../../../data/ConceptAbbreviations';
 
 /**
  * WorldView is the world view for the app, where the user can see all the
@@ -18,14 +19,31 @@ class WorldView extends Component {
 	 * @returns {Array.<*>}
 	 */
 	getOrderedConcepts(): ConceptKnowledge[] {
-		return MasteryModel.model.filter((concept) => concept.teach && concept.container).sort(
+		return MasteryModel.model.filter((concept) => concept.should_teach && concept.container).sort(
 				(a, b) => (b.dependencyKnowledge / b.knowledge -
 						a.dependencyKnowledge / a.knowledge));
 	}
 
+  /**
+   * getConceptsByType takes the orderedConcepts and then grabs only the ones with the 
+   * specified type
+   * @param {Array.<*>} orderedConcepts concepts from getOrderedConcepts();
+   * @param {string} type type to filter by
+   */
+  getConceptsByType(orderedConcepts: ConceptKnowledge[], type: string) {
+    console.log(orderedConcepts);
+    return orderedConcepts.filter(concept => {
+      return concept.type === type;
+    })
+  }
 
   render() {
     let conceptList = this.getOrderedConcepts();
+    let title = {
+      [t.onboarding] : "Intro",
+      [t.template] : "Templates",
+      [t.semantic] : "Code constructs"
+    }
 
     return (
         <div className="container" style={{marginTop: '12vh'}}>
@@ -33,7 +51,24 @@ class WorldView extends Component {
 					<li>Here you'll learn to code</li>
 					<li>How code runs</li>
 					<h1 style={{marginTop: "5vh"}}>Concepts</h1>
-					{
+          {
+            Object.keys(t).map(cTypeVal => {
+              let cType = t[cTypeVal];
+              return <div key={"world-"+cType}>
+                <h2>{title[cType]}</h2>
+                {this.getConceptsByType(conceptList, cType).map((concept, index) => {
+                  let name = conceptInventory[concept.name].explanations.name;
+							    return <ConceptCard title={name}
+																	key={index}
+																	concept={concept.name}
+																	generateExercise={this.props.generateExercise}
+																	getInstruction={this.props.getInstruction}/>
+                })}
+              </div>
+            })
+
+          }
+					{/* {
 						conceptList.map((concept, index) => {
 							let name = conceptInventory[concept.name].explanations.name;
 							return <ConceptCard title={name}
@@ -42,7 +77,7 @@ class WorldView extends Component {
 																	generateExercise={this.props.generateExercise}
 																	getInstruction={this.props.getInstruction}/>
 						})
-					}
+					} */}
 				</div>
 		);
 	}
