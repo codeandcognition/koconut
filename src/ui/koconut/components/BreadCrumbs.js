95c9058382ff64f085c94d66189b30200887de0f
Jane <janeq97@uw.edu>
2018-08-07 10:37:02 -0700

    Added concept lists to breadcrumb class

--- src/ui/koconut/components/BreadCrumbs.js
+++ src/ui/koconut/components/BreadCrumbs.js
@@ -1,22 +1,133 @@
 // @flow
 import React, {Component} from 'react';
+import {t} from '../../../data/ConceptAbbreviations';
+import {ConceptKnowledge, MasteryModel} from '../../../data/MasteryModel';
+import {conceptInventory} from '../../../data/ConceptMap.js';
+
+type Props = {
+  conceptType: string,
+  chosenInstruction: any
+}
 
 export default class BreadCrumbs extends Component {
+
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      concept: '',
+      orderedConcepts: null,
+      semanticConcepts: null,
+      templateConcepts: null,
+      onboardingConcepts: null
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      concept: this.formatCamelCasedString(this.props.conceptType),
+      orderedConcepts: this.getOrderedConcepts()
+    }, () => {
+
+      var semantic = [];
+      var template = [];
+      var onboarding = [];
+
+      this.getConceptsByType(this.state.orderedConcepts, t.semantic).map((item) => {
+        semantic.push(item.name);
+      });
+
+      this.getConceptsByType(this.state.orderedConcepts, t.template).map((item) => {
+        template.push(item.name);
+      });
+
+      this.getConceptsByType(this.state.orderedConcepts, t.onboarding).map((item) => {
+        onboarding.push(item.name);
+      });
+
+      this.setState({
+        semanticConcepts: semantic,
+        templateConcepts: template,
+        onboardingConcepts: onboarding
+      });
+    });
+  }
+
+  /**
+   * This function takes in a camel cased string and converts it to normal
+   * text with the first letter of every word being capitalized.
+   * @param camelString
+   * @returns {string}
+   */
+  formatCamelCasedString(camelString: string) {
+    var result = "";
+    if (camelString.length !== 0) {
+      result = result + camelString.charAt(0).toUpperCase();
+      for (var i = 1; i < camelString.length; i++) {
+        if (camelString.charAt(i) === camelString.charAt(i).toUpperCase()) {
+          result = result + " "
+        }
+        result = result + camelString.charAt(i);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Returns sorted concepts list sorted by relevance to the user.
+   * Only includes concepts where concept.teach is true and concept.container
+   * is false
+   * @returns {Array.<*>}
+   */
+  getOrderedConcepts(): ConceptKnowledge[] {
+    return MasteryModel.model.filter((concept) => concept.should_teach && concept.container).sort(
+        (a, b) => (b.dependencyKnowledge / b.knowledge -
+            a.dependencyKnowledge / a.knowledge));
+  }
+
+  /**
+   * getConceptsByType takes the orderedConcepts and then grabs only the ones with the
+   * specified type
+   * @param {Array.<*>} orderedConcepts concepts from getOrderedConcepts();
+   * @param {string} type type to filter by
+   */
+  getConceptsByType(orderedConcepts: ConceptKnowledge[], type: string) {
+    return orderedConcepts.filter(concept => {
+      return concept.type === type;
+    })
+  }
+
   render() {
     let readOrWrite = "";
-    if(this.props.readOrWrite === "READ") {
-      readOrWrite = "read instructions";
+    if (this.props.readOrWrite === "READ") {
+      readOrWrite = "Learn to Read Code";
     } else {
-      readOrWrite = "write instructions";
+      readOrWrite = "Learn to Write Code";
     }
-    return (<div>
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item"><a href="#">{this.props.conceptType}</a></li>
-          <li className="breadcrumb-item"><a href="#">{readOrWrite}</a></li>
-          <li className="breadcrumb-item active" aria-current="page">{this.props.chosenInstruction ? this.props.chosenInstruction.title : ""}</li>
-        </ol>
-      </nav></div>)
+
+    var conceptMenu = [];
+    if (this.state.semanticConcepts && this.state.semanticConcepts.includes(this.props.conceptType)) {
+      conceptMenu = this.state.semanticConcepts;
+    } else if (this.state.templateConcepts && this.state.templateConcepts.includes(this.props.conceptType)) {
+      conceptMenu = this.state.templateConcepts;
+    } else if (this.state.onboardingConcepts && this.state.onboardingConcepts.includes(this.props.conceptType)) {
+      conceptMenu = this.state.onboardingConcepts;
+    }
+
+    console.log(conceptMenu);
+
+    return (
+      <div>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item"><a href="#">{this.state.concept}</a></li>
+            <li className="breadcrumb-item"><a href="#">{readOrWrite}</a></li>
+            <li className="breadcrumb-item active" aria-current="page">{this.props.chosenInstruction ? this.props.chosenInstruction.title : ""}</li>
+          </ol>
+        </nav>
+      </div>
+    );
   }
 }
 
