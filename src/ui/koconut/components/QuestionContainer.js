05a5946f995dbbc602e6d05ce79bc24d2399c5ff
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-09-11 12:11:42 -0700

    Wrapper around ExerciseQuestion to display quadrant info and answer #151

---
+++ src/ui/koconut/components/QuestionContainer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+type Props = {
+	// TODO: Add to this
+};
+
+class QuestionContainer extends Component {
+	render() {
+		return(
+				<div>
+					<p>Quadrant Info</p>
+					<p>Exercise Question</p>
+					<p>Answer</p>
+				</div>
+		);
+	}
+}
+
+export default QuestionContainer;
