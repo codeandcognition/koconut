f3bdb8096f1289b16c80b84ea7093bf2e08fef53
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-09-11 12:10:31 -0700

    Added child components to AllExercises.js #151

--- src/ui/koconut/containers/AllExercises.js
+++ src/ui/koconut/containers/AllExercises.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import { withRouter} from "react-router-dom";
 
+import ExerciseInfoContainer from './../components/ExerciseInfoContainer';
+
 class AllExercises extends Component {
 	render() {
 		return (
 				<div className={"container"}>
 					Display All exercises here
+					<ExerciseInfoContainer/>
+					<br/>
+					<ExerciseInfoContainer/>
 				</div>
 		);
 	}
