f2a4c9189021376ccc08381285493ab19dd34e63
Jane <janeq97@uw.edu>
2018-09-11 11:48:31 -0700

    Added getAllExercies function

--- src/ui/koconut/containers/AllExercises.js
+++ src/ui/koconut/containers/AllExercises.js
@@ -1,7 +1,32 @@
 import React, {Component} from 'react';
 import { withRouter} from "react-router-dom";
+import firebase from 'firebase';
 
 class AllExercises extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			allExercises: []
+		}
+	}
+
+	componentDidMount() {
+		this.getAllExercises();
+	}
+
+	getAllExercises() {
+	  let componentRef = this;
+		let databaseRef = firebase.database().ref("Exercises");
+		databaseRef.on("value", function(snapshot) {
+		  let exercises = snapshot.val();
+			componentRef.setState({
+				allExercises: exercises
+			});
+		});
+	}
+
+
 	render() {
 		return (
 				<div className={"container"}>
