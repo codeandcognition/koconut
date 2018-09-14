dbd2a0b849cb46e8f00675ca2c90cf29b69c3cfb
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-09-14 16:23:16 -0700

    Closes #168

--- src/backend/ExerciseGenerator.js
+++ src/backend/ExerciseGenerator.js
@@ -40,13 +40,15 @@ class ExerciseGenerator {
     let results = [];
     let exerciseIds = [];
     if(exerciseList && conceptMapGetter) {
-      conceptMapGetter[concept].forEach((exerciseId) => {
-        if ((exerciseType === "READ" && exerciseList[exerciseId] && ExerciseTypes.isReadType(exerciseList[exerciseId].questions[0].type)) ||
-            (exerciseType === "WRITE" && exerciseList[exerciseId] && !ExerciseTypes.isReadType(exerciseList[exerciseId].questions[0].type) )) {
-          results.push(exerciseList[exerciseId]);
-          exerciseIds.push(exerciseId);
-        }
-      })
+    	if (conceptMapGetter[concept]) {
+				conceptMapGetter[concept].forEach((exerciseId) => {
+					if ((exerciseType === "READ" && exerciseList[exerciseId] && ExerciseTypes.isReadType(exerciseList[exerciseId].questions[0].type)) ||
+							(exerciseType === "WRITE" && exerciseList[exerciseId] && !ExerciseTypes.isReadType(exerciseList[exerciseId].questions[0].type) )) {
+						results.push(exerciseList[exerciseId]);
+						exerciseIds.push(exerciseId);
+					}
+				});
+			}
     }
     return {results, exerciseIds};
   }
