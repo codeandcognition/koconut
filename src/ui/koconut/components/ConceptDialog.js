33bb8cf49ffbbec47fb441357b281b5ac1105f0a
Jane <janeq97@uw.edu>
2018-09-18 12:24:57 -0700

    Added examples and overview heading to dialog box

--- src/ui/koconut/components/ConceptDialog.js
+++ src/ui/koconut/components/ConceptDialog.js
@@ -5,6 +5,9 @@ import DialogContent from '@material-ui/core/DialogContent';
 import Button from '@material-ui/core/Button';
 import './ConceptDialog.css';
 import ConceptInventory from './../../../data/ConceptMap';
+import ReactMarkdown from 'react-markdown';
+import CodeBlock from './CodeBlock';
+import firebase from 'firebase';
 
 type Props = {
 	title: string,
@@ -35,7 +38,21 @@ class ConceptDialog extends Component {
 		});
 	}
 
+  renderMarkdown(codeString: string, index: number) {
+    let code = "```python\n" + codeString + "\n```";
+    return <ReactMarkdown className={"flex-grow-1"}
+													key={index}
+                          source={code}
+                          renderers={{code: CodeBlock}}
+                          escapeHtml={true}
+    />
+  }
+
+
+
 	render() {
+		let conceptInfo = ConceptInventory[this.props.conceptCode].explanations
+
 		return (
 				<Dialog open={this.state.open} onClose={this.handleClose}>
 					<DialogContent>
@@ -43,13 +60,20 @@ class ConceptDialog extends Component {
 							<h2>{this.props.title}</h2>
 							<i className="far fa-times-circle icon" onClick={() => this.handleClose()}/>
 						</div>
-						<p>
-							{ConceptInventory[this.props.conceptCode].explanations.definition}
-						</p>
-						<div>
-							<Button color={"primary"} variant={'outlined'} className={'link'}>Learn</Button>
-							<br/>
-							<Button color={"primary"} variant={'outlined'} className={'link'}>Practice</Button>
+						<p>{conceptInfo.definition}</p>
+						<p><span className={"bold-text"}>Examples</span></p>
+						{conceptInfo.examples.map((item, index) => {
+              return this.renderMarkdown(item, index);
+            })}
+						<h4>Overview</h4>
+						<div className={"overview-container"}>
+							<div>
+								<p>Reading Code</p>
+
+							</div>
+							<div>
+								<p>Writing Code</p>
+							</div>
 						</div>
 					</DialogContent>
 				</Dialog>
