1ee3e0ed4786d3e27c4be35982d0bf2cc3e93ad8
William Kwok <wkwok16@uw.edu>
2018-11-05 16:50:22 -0800

    upd datalogger test

--- src/tests/dataLoggers/DataLogger.test.js
+++ src/tests/dataLoggers/DataLogger.test.js
@@ -21,7 +21,8 @@ describe('DataLogger tests', () => {
 
   it('addData throws an error if selectedAnswer is not provided', () => {
     let d = new DataLogger('READ');
-    expect(() => {d.addData('MOUSECLICK', 'M1', 'asdfsasdf', {row: 1, col: 2})}).toThrow();
+    let paramsWithoutSelectedAnswer = ['MOUSECLICK', 'M1', 'asdfsasdf', {row: 1, col: 2}];
+    expect(() => {d.addData(...paramsWithoutSelectedAnswer)}).toThrow();
   });
 
   it('addData works for WRITE types', () => {
@@ -38,5 +39,25 @@ describe('DataLogger tests', () => {
     expect(d.getData().length).toBe(1);
     expect(d.getData()[0].selectedAnswer).toBeUndefined();
   });
-  
+
+  it('addData timestamps rough value correctly', () => {
+    const event = 'MOUSECLICK';
+    const keyPressed = 'M1';
+    const textContent = 'asdfasdf';
+    const textPosition = {row: 1, col: 2};
+    const selectedAnswer = 1;
+    let d = new DataLogger('READ');    
+    const timestamp1 = Date.now();
+    d.addData(event, keyPressed, textContent, textPosition, selectedAnswer);
+    const timestamp2 = Date.now();
+
+    let e = d.getData()[0];
+
+    expect(e.event).toBe(event);
+    expect(e.keyPressed).toBe(keyPressed);
+    expect(e.textContent).toBe(textContent);
+    expect(e.textPosition).toEqual(textPosition);
+    expect(e.timestamp).toBeGreaterThanOrEqual(timestamp1);
+    expect(e.timestamp).toBeLessThanOrEqual(timestamp2);
+  })
 })
