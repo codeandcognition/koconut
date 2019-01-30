485095ce655638e73765b53b67d581524d769128
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-01-30 12:36:14 -0800

    Revert "Minimal UI for displaying the hints"
    This reverts commit 831e4f1badbcbfb77f6fa8dcc1027f14c1196665.

--- src/ui/koconut/components/Hint.js
+++ src/ui/koconut/components/Hint.js
@@ -1,11 +1,6 @@
 // @flow
 import React, {Component} from 'react';
-import './Submit.css';
-
-type Props = {
-	hintRequestHandler: Function,
-  disableHint: boolean
-};
+import './Hint.css';
 
 /**
  * Component displays a hint at code editor's last cursor position.
@@ -13,13 +8,32 @@ type Props = {
 class Hint extends Component {
 
   render() {
+    let content: string = this.props.content;
+    let pos = this.props.pos;
+    let hintW = this.props.width ? this.props.width : 100; // Default width
+    let hintH = this.props.height ? this.props.height : 100; // Default height
+    let pad = this.props.padding ? this.props.padding : 10; // Default padding
+
+    //let resources = this.props.resources;
+
     return (
-        <div className={"submit-container"}>
-					<button className={"btn btn-submit click"} disabled={this.props.disableHint} onClick={() => {
-						this.props.hintRequestHandler()
-					}}>
-						Get Hint
-					</button>
+        <div className="hint" style={{
+          top: pos + 'px',
+          left: -1 * (hintW + 3 * pad) + 'px',
+          width: hintW + 'px',
+          height: hintH + 'px',
+          padding: pad + 'px',
+        }}>
+          <div className="body-text">
+            {content}
+          </div>
+          {/*<div className="list-resources">*/}
+            {/*{resources.map(*/}
+                {/*(resources) => (<div className="resource-item">resource</div>))}*/}
+          {/*</div>*/}
+          <div className="close-hint"
+               onClick={(this.props.close) ? (this.props.close) : {}}>x
+          </div>
         </div>
     );
   }
