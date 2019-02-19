92711b216d21915bf43e84e6030d0524bc449326
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-02-19 15:30:03 -0800

    New component for button within side navigation

---
+++ src/ui/koconut/components/NavItem.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import './NavItem.css';
+
+class NavItem extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: props.name,
+			read: props.read,
+			suggestionText: props.suggestionText || '',
+		}
+	}
+
+	componentWillReceiveProps({ name, read, suggestionText}) {
+		this.setState({ name, read, suggestionText});
+	}
+
+	render() {
+		let complete = this.state.read ? { color: "#80B948" } : { color: "grey"};
+		let highlight = this.state.suggestionText ? { borderLeft: "5px solid #4054B2"} : {};
+		return(
+				<div>
+					<ListItem className={"nav-item"} style={highlight}>
+						{this.state.suggestionText && <p className={"recommendation"}>{this.state.suggestionText}</p>}
+						<div className={"nav-item-info"}>
+							<ListItemText className={"nav-item-text"} primary={this.state.name} />
+							<div className={"item-status"} style={complete}><i className={"fas fa-check"} /></div>
+						</div>
+					</ListItem>
+				</div>
+		);
+	}
+}
+
+export default NavItem;
