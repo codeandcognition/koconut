f029df27562467746e83c8f1baed8cbaffa77884
kwokwilliam <wkwok16@uw.edu>
2019-03-22 12:53:53 -0700

    Fixed import order in __init__ and now it runs

--- python_api/app/__init__.py
+++ python_api/app/__init__.py
@@ -1,4 +1,5 @@
-from app import routes
 from flask import Flask
 
 app = Flask(__name__)
+
+from app import routes
--- python_api/run_python.py
+++ python_api/run_python.py
@@ -1,7 +1,8 @@
 import os
+from app import app
 
 HOST = os.getenv("HOST", "localhost")
 PORT = os.getenv("PORT", 8080)
 
 if __name__ == "__main__":
-    print("abc")
+    app.run(host=HOST, port=PORT)
