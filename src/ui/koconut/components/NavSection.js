c62fc23e5ea0c8e184e47299c67934db0a07d5af
Benji Xie <bxie@UW.edu>
2019-07-09 12:04:02 -0700

    UI to show selected item in sideNav (#328)
    involved creating state.selectedIndex in App.js and propogating that down to NavItem and comparing to index. state.selectedIndex changes on getInstruction(), goToExercise(), and reset on switchToWorldView()

--- src/ui/koconut/components/NavSection.js
+++ src/ui/koconut/components/NavSection.js
@@ -3,7 +3,6 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Progress from './Progress';
 
 type Props = {
     defaultExpanded: Boolean
