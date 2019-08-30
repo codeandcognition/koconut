38103718ab48fa3f1289ca56ad8deb6388d1ccc4
bxie <bxie@uw.edu>
2019-08-29 19:39:17 -0700

    removed extra lines

--- src/ui/koconut/components/ConceptOverview.js
+++ src/ui/koconut/components/ConceptOverview.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import ReactMarkdown from 'react-markdown';
 import CodeBlock from './CodeBlock';
 import ConceptInventory from '../../../data/ConceptInventory';
